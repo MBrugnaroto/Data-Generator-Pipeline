e63ad500f6ae1f8902d84da53fba3be72b7e8204
mbrugnaroto <brugnaroto@gmail.com>
2022-05-28 15:35:42 -0300

    refact: method return type definition and code standardization

--- data-generator/mariadb_hook.py
+++ data-generator/mariadb_hook.py
@@ -1,17 +1,24 @@
-import os
 import pandas as pd
 import mysql.connector as MariaDB
 from utils import handle_query as HQ
+from typing import List
+
 
 class MariaDBHook():
-    def __init__(self, database=None, host=None, port=None, user=None, password=None) -> None:
-        self.database = database or "DB_TEST"
-        self.host = host or "127.0.0.1"
-        self.port = port or 3306
-        self.user = user or os.environ.get("USER") or 'root'
-        self.pw = password or os.environ.get('PASSWORD') or '' 
+    def __init__(self, 
+                 database=None, 
+                 host=None, 
+                 port=None, 
+                 user=None, 
+                 password=None) -> None:
+        self.database = database
+        self.host = host
+        self.port = port 
+        self.user = user 
+        self.pw = password
         
-    def get_conn(self):
+        
+    def get_conn(self) -> MariaDB.connect:
         return MariaDB.connect(
             host=self.host,
             user=self.user,
@@ -20,7 +27,8 @@ class MariaDBHook():
             port=self.port
         )
         
-    def get_tables(self):
+        
+    def get_tables(self) -> List:
         conn = self.get_conn()
         cursor = conn.cursor()
         cursor.execute("SHOW TABLES;")
@@ -29,17 +37,20 @@ class MariaDBHook():
         conn.close()
         return tables
 
-    def bulk_dump(self, table):
+
+    def bulk_dump(self, table) -> pd.DataFrame:
         conn = self.get_conn()
         dump = pd.read_sql(HQ.get_select_table_sql(table), conn)
         conn.close()
         return dump
     
-    def bulk_load(self, path_file, table):
+    
+    def bulk_load(self, path_file, table) -> None:
         conn = self.get_conn()
         cursor = conn.cursor()
         bulk_query = HQ.upload_data_from_file(path_file, table)
         cursor.execute(bulk_query)
-        cursor.commit()
+        conn.commit()
         cursor.close()
         conn.close()
+        
