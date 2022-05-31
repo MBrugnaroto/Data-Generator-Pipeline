4a8950fcdc0910102503a7b960487482ac2ff2f0
mbrugnaroto <brugnaroto@gmail.com>
2022-05-31 18:07:08 -0300

    feact: implementation of framework for evaluation

---
+++ data-generator/app/dags/invoices_dag.py
@@ -0,0 +1,56 @@
+import os
+from os.path import join
+from pathlib import Path
+from datetime import datetime
+from plugins.operators.invoice_generator_operator import InvoiceGeneratorOperator
+from plugins.operators.invoice_import_operator import InvoiceImportGenerator
+from plugins.hooks.mariadb_hook import MariaDBHook
+
+
+SOURCE_FOLDER=str(Path(__file__).parents[2])
+PATH_DATALAKE=join(
+            SOURCE_FOLDER,
+            "datalake",
+            "{layer}",
+            "{db}",           
+)
+
+
+def get_connection():
+    return MariaDBHook(
+            database="DB_TEST",
+            host="127.0.0.1",
+            port=3306,
+            user=os.environ.get("USER"),
+            password=os.environ.get('PASSWORD')
+        )
+
+
+def invoice_dag(id):
+    hook = get_connection()
+    
+    generator_start = datetime.now()
+    InvoiceGeneratorOperator(
+        max_invoices=100000, 
+        max_items=1,
+        quantity=10,
+        path_file=PATH_DATALAKE\
+                    .format(layer="silver", db="DB_TEST"),
+        id=id)\
+        .executor()
+    generator_end = datetime.now()
+    total = (generator_end-generator_start).total_seconds()
+    
+    hook.update_data("round_statistics", total, id, "generator")
+    
+    import_start = datetime.now()
+    InvoiceImportGenerator(
+        path_dump=PATH_DATALAKE\
+                    .format(layer="silver", db="DB_TEST"),
+        id=id)\
+        .executor()
+    import_end = datetime.now()
+    total = (import_end-import_start).total_seconds()
+    
+    hook.update_data("round_statistics", total, id, "import")
+    
---
+++ data-generator/app/plugins/utils/jinja_templates.py
@@ -0,0 +1,48 @@
+_SIMPLE_SELECT_TEMPLATE = \
+    """
+    {% if show_columns %}
+        {% set columns = '\n    ,'.join(show_columns) %}
+    {% endif %}
+    SELECT
+        {% if columns %}
+            {{ columns | sqlsafe }}
+        {% else %}
+            * 
+        {% endif %}
+    FROM 
+        {{ table | sqlsafe }}
+    """
+    
+_UPLOAD_FROM_FILE = \
+    """
+    LOAD DATA INFILE {{ path_file }}
+    INTO TABLE {{ table | sqlsafe}}
+    FIELDS TERMINATED BY ','
+    LINES TERMINATED BY '\n'
+    """
+    
+_INSERT_DATA = \
+    """
+    {% if in_columns %}
+        {% set columns = ', '.join(in_columns) %}
+    {% endif %}
+    INSERT INTO {{ table | sqlsafe }} 
+    {% if columns %}
+        ({{ columns | sqlsafe }})
+    {% endif %}
+    VALUES {{ values | inclause}}
+    """
+    
+_UPDATE_DATE = \
+    """
+    UPDATE {{ table | sqlsafe}}
+    SET 
+        operator_total_time = {{ value }}
+    WHERE
+        id={{ id }} AND operator={{ operator }} 
+    """
+    
+_TRUNCATE_TABLE = \
+    """
+    TRUNCATE TABLE {{ table | sqlsafe}}
+    """
