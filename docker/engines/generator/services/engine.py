6fe9e70e41dda5ebfcdad63de5c18e5603f7324c
mbrugnaroto <brugnaroto@gmail.com>
2022-06-28 23:36:54 -0300

    feat: continue

---
+++ docker/engines/generator/services/engine.py
@@ -0,0 +1,20 @@
+from ast import arg
+from dags import generator_dag
+import argparse
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(
+        description="Invoice Generator"
+    )
+
+    parser.add_argument("--max-invoices", required=True)
+    parser.add_argument("--max-items", required=True)
+    parser.add_argument("--quantity", required=True)
+    args = parser.parse_args()
+    
+    generator_dag\
+        .execute_dag(
+            max_invoices=args.max_invoices,
+            max_items=args.max_items,
+            quantity=args.quantity
+    )
