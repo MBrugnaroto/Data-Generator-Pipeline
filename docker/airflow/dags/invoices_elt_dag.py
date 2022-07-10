71703125d26b546c7f65a1f12a0286ed6eb799e0
mbrugnaroto <brugnaroto@gmail.com>
2022-07-10 17:28:08 -0300

    feact: invoice generator task set

--- docker/airflow/dags/invoices_elt_dag.py
+++ docker/airflow/dags/invoices_elt_dag.py
@@ -1,6 +1,6 @@
 from airflow.decorators import dag
 from airflow.operators.docker_operator import DockerOperator
-
+from docker.types import Mount
 from datetime import datetime
 
 ENV = {
@@ -14,17 +14,23 @@ ENV = {
     'QUANTITY': 100
 }
 
+VOLUME = [
+    Mount(target="/usr/src/app/datalake", source="/home/mbrugnar/workspace/data-generator/docker/datalake", type='bind')
+]
+
 @dag(start_date=datetime(2022, 7, 9), schedule_interval='@daily', catchup=False)
 def generator_dag():
     t1 = DockerOperator(
-        task_id='task2',
+        task_id='invoice_generator',
         image='mbrugnar/generator:latest',
         container_name='generator_container',
         environment=ENV,
         force_pull=True,
-        auto_remove=True
+        auto_remove=True,
+        network_mode="database_connect_network",
+        mounts=VOLUME
     )
     
     t1
-
+    
 dag=generator_dag()
