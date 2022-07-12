5b1a910860be28e5929422123b83d65c9bc8edaf
mbrugnaroto <brugnaroto@gmail.com>
2022-07-12 10:17:37 -0300

    feact: structure update

--- docker/airflow/include/images/engines/generator/services/execute.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-python3 engine.py --max-invoices ${MAX_INVOICES} --max-items ${MAX_ITEMS} --quantity ${QUANTITY}
--- docker/stop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker-compose -f airflow/docker-compose.yml down -v
-docker-compose -f database/docker-compose.yml down -v
